2f43387409fc4b43cfaf9a1a00753d563647c887
hansjensen <grenademisfits@gmail.com>
2022-12-17 21:08:00 -0800

    kinda works not really tho

--- rps.js
+++ rps.js
@@ -1,49 +1,110 @@
+//press start
+
+let comp = 0;
+let you = 0;
+let message = document.querySelector('#message');
+const start = document.querySelector('#start');
+let compsel = document.querySelector('#compsel');
+let playsel = document.querySelector('#playsel');
+let compscore = document.querySelector('#compscore');
+let playscore = document.querySelector('#playscore');
+let startdiv = document.querySelector('.start');
+let rock = document.createElement('div');
+    rock.setAttribute('id', 'rock');
+    rock.classList.add('rps', 'click', 'rock1');
+let paper = document.createElement('div');
+    paper.setAttribute('id', 'paper');
+    paper.classList.add('rps', 'click', 'paper1');
+let scissors = document.createElement('div');
+    scissors.setAttribute('id', 'scissors');
+    scissors.classList.add('rps', 'click', 'scissors1');
+let choice = document.querySelector('#choice');
 
 
 
-
-
-function ok() {
-    let playerSelection = "rock";
-    let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-}
-
-function game() {
-    let comp = 0;
-    let you = 0;
-   for (let i = 0; i < 5; i++){
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Rock paper scissors?")
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
+start.addEventListener('click', () => {
+    choice.appendChild(rock);
+    choice.appendChild(paper);
+    choice.appendChild(scissors);
+    comp= 0;
+    you = 0;
+    message.textContent = "Rock paper or scissors?"
+    playscore.textContent = you;
+    compscore.textContent = comp;
+    playsel.classList = "";
+    compsel.classList = "";
+    start.remove();
     
-    if (result[4] === 'w') {
-        you++;
-    } else {
-        comp++
-    }
-   }
-   if (you > comp) {
-    console.log('You Win!')
-   } else {
-    console.log('You lose!')
-   }
+    
 
-}
+});
+
+
+
+function gamePlay() {
+    const rps = document.querySelectorAll('.rps')
+    rps.forEach((div) => {
+        div.addEventListener('click', () => {
+            playsel.classList = "";
+            compsel.classList = "";
+            let playerSelection = div.id;
+            let computerSelection = getComputerChoice();
+            let result = playRound(playerSelection, computerSelection);
+            playsel.classList.add(playerSelection);
+            compsel.classList.add(computerSelection);
+            message.textContent = result;
+       
+        if (result[4] === 'w') {
+             you++;
+        } else if (result[4] === 'l'){
+            comp++;
+        } 
+
+        playscore.textContent = you;
+        compscore.textContent = comp;
+
+    if (you === 5) {
+            rock.remove();
+            paper.remove();
+            scissors.remove();
+            message.textContent = "YOU WIN!";
+            startdiv.appendChild(start);
+            return;
+     } else if (comp === 5) {
+        rock.remove();
+        paper.remove();
+        scissors.remove();
+        message.textContent = "YOU LOSE!";
+        startdiv.appendChild(start);
+        return;
+       }
+       
+      
+    
+        
+        });
+    });
+    
+    
+};
+
+
+
+
+
 
 
 function playRound(playerSelection, computerSelection) {
-    let pSelect = playerSelection.toLowerCase ()
-    if (pSelect === computerSelection)
+    
+    if (playerSelection === computerSelection)
         return 'Tie! ' + playerSelection.toUpperCase() + ' ties ' + computerSelection.toUpperCase() + " !";
-    if (pSelect === 'rock') {
+    if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
             return 'You win! ROCK beats SCISSORS!';
         } else {
             return 'You lose! PAPER beats ROCK! '
         }
-    } else if (pSelect === 'paper') {
+    } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             return 'You win! PAPER beats ROCK!';
         } else {
@@ -70,4 +131,4 @@ function getComputerChoice() {
     } else {
         return 'paper';
     }
-}
+};
